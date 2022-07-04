aa49982aec64c7f9046f8a8f363dc924c73a774a
Airton Canale <airtonluizcanalefilho@hotmail.com>
2022-07-04 10:45:23 -0300

    busca de pacientes

--- js/filtra.js
+++ js/filtra.js
@@ -2,10 +2,11 @@ var campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function () {
   document.querySelectorAll(".paciente").forEach((paciente) => {
-    console.log(this.value);
     var tdNome = paciente.querySelector(".info-nome");
     var nome = tdNome.textContent;
-    if (!nome.includes(this.value)) {
+    var expressao = new RegExp(this.value, "i")
+    // if (!nome.includes(this.value,"i")) {
+      if (!expressao.test(nome)) {
       paciente.classList.add("invisivel");
     } else {
       paciente.classList.remove("invisivel");
@@ -13,14 +14,26 @@ campoFiltro.addEventListener("input", function () {
   });
 });
 
-  // for (var i = 0; i < pacientes.lentght ; i++){
-  // var paciente = pacientes [i]
-  // var tdNome = paciente.querySelector(".info-nome")
-  // var nome = paciente.textContent;
-  //      if (nome != this.value) {
-  //       paciente.classList.add("invisivel");
-  //     } else {
-  //       paciente.classList.remove("invisivel");
-  //     }
-  //   }
-  
+// var campoFiltro = document.querySelector("#filtrar-tabela");
+
+// campoFiltro.addEventListener("input", function () {
+//   var pacientes = document.querySelectorAll(".paciente");
+//   if (this.value.length > 0) {
+//     for (var i = 0; i < pacientes.length; i++) {
+//       var paciente = pacientes[i];
+//       var tdNome = paciente.querySelector(".info-nome");
+//       var nome = tdNome.textContent;
+//       var expressao = new RegExp(this.value, "i");
+//       if (!expressao.test(nome)) {
+//         paciente.classList.add("invisivel");
+//       } else {
+//         paciente.classList.remove("invisivel");
+//       }
+//     }
+//   } else {
+//     for (var i = 0; i < pacientes.length; i++) {
+//       var paciente = pacientes[i];
+//       paciente.classList.remove("invisivel");
+//     }
+//   }
+// });
